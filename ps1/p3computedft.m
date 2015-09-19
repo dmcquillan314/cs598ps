e5f7f7cd1f2507c89335427af2202ebb4f94ca47
Daniel McQuillan <dmcquillan@ahold.com>
2015-09-18 22:27:58 -0500

    fix stupid issue

--- ps1/p3computedft.m
+++ ps1/p3computedft.m
@@ -1,7 +1,22 @@
 function [ T ] = p3computedft( M )
 
-    F = fft(eye(size(M,1)));
-    T = F * M * F;
+    N = size(M,1);
+    normN = sqrt(N);
     
+    x = 0:N-1;
+    y = 0:N-1;
+    
+    F = zeros(N,N);
+
+    for j = 1:N
+        for k = 1:N
+            inner_part = (x(j) * y(k) * 2 * pi) / N;
+            cos_part = cos(inner_part);
+            sin_part = 1i * sin(inner_part);
+            F(j, k) = (1 / normN) * (cos_part + sin_part);
+        end
+    end
+    
+    T = F * M * F; 
 end
 
