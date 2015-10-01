a6d4a80fd4e0448852f7aca5cadb5ce1a0629291
Daniel McQuillan <dmcquillan@ahold.com>
2015-09-30 23:39:29 -0500

    Add p6.22

--- ps2/l6_22.m
+++ ps2/l6_22.m
@@ -1,32 +1,50 @@
 function l6_22()
     figure;
-    r1 = rand_uniform_points(-1,1,100);
-    r2 = rand_uniform_points(-1,1,100);
+    r1 = rand_uniform_points(-1,1,1000);
+    r2 = rand_uniform_points(-1,1,1000);
     x = 2 * r1 + r2;
     y = r1 + r2;
     
-    Z = [x y];
+    X = [x y];
     
-    subplot(1,2,1);
+    subplot(2,2,1);
     plot(x,y,'.');
     
+    P = compute_pca(X);
+    
+    subplot(2,2,2);
+    plot(P(:,1),P(:,2),'.');
+   
     eta = 1e-5;
     eps = 1e-7;
     
-    N = size(Z,1);
-    W = eye(N);
- 
-    for k = 1:10
-        Y = Wi * x;
-        del_W = ((N * eye(N)) - (Y.^3 * y'))*W;
+    N = size(X,1);
+    W = eye(2);    
+    
+    k = 0;
+    err = inf;
+    maxIters = 130;
+    [d n] = size(P);
+    
+    while (k < maxIters)
+        k = k + 1;
+        Wlast = W;
+        
+        Y = X * W;
+        
+        del_W = ((N * eye(2)) - (Y' .^ 3) * Y)*W;
         W = W + eta * del_W;
+        
+        err = max(1 - dot(W,Wlast,2));
+        disp(err);
     end
     
-    Y = W * x;
-    subplot(1,2,2);
+    Y = X * W;
+    subplot(2,2,4);
     plot(Y(:,1),Y(:,2),'.');
 end
 
-function [x] = kernel_22(x)
-    z = x.^3;
+function [P] = compute_pca(x)
+    [V D] = eig(cov(x));
+    P = x * V;
 end
